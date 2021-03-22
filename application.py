2b89f8b8b8c72d01a204a945c6d163b2f9afaf06
Tebby Thomas <tebby.thomas@gmail.com>
2021-03-22 13:12:37 +0530

    Editing files

--- application.py
+++ application.py
@@ -1,12 +1,16 @@
 from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello World Again!'
+application = Flask(__name__)
 
 
-if __name__ == '__main__':
-    app.run()
+@application.route("/")
+def index():
+    return "Your Flask App Works!"
+
+
+@application.route("/hello")
+def hello():
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    application.run(port=5000, debug=True)
--- test_application.py
+++ test_application.py
@@ -1,5 +1,9 @@
-from application import app
-with app.test_client() as c:
+from application import application
+with application.test_client() as c:
     response = c.get('/')
-    assert response.data == b'Hello World Again!'
+    assert response.data == b'Your Flask App Works!'
     assert response.status_code == 200
+    response2 = c.get('/hello')
+    assert response2.data == b'Hello World!'
+    assert response2.status_code == 200
+
