480839a2a8ef025b5ca1b792dfe6e24fcaef4b0b
Tebby Thomas <tebby.thomas@gmail.com>
2021-03-22 11:18:47 +0530

    Editing the returned message

--- test_app.py
+++ test_app.py
@@ -1,5 +1,5 @@
 from app import app
 with app.test_client() as c:
     response = c.get('/')
-    assert response.data == b'Hello World!'
+    assert response.data == b'Hello World Again!'
     assert response.status_code == 200
